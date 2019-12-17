a578a444e10503b8aec1316bcb6290383f203379
Nicolas Guarini <nikiguarincia@gmail.com>
2019-12-18 00:18:14 +0100

    Start adding snake's right movement, but the snake stretches instead of moving

--- SnakeGame.xaml.cs
+++ SnakeGame.xaml.cs
@@ -12,17 +12,69 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Diagnostics;
 
 namespace snake_wpf
 {
-    /// <summary>
-    /// Logica di interazione per SnakeGame.xaml
-    /// </summary>
     public partial class SnakeGame : Page
     {
+        List<Point> snakePartsCoordinates = new List<Point>(); //list that will contain the coordinates of every single snake part
+        const int snakeSize = 10; //width in px of the snake's head
+        string direction = "right";
+        
+
+        DispatcherTimer timer = new DispatcherTimer(); 
         public SnakeGame()
         {
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromMilliseconds(150);
+            SetInitialParts();
+
             InitializeComponent();
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            foreach (Point i in snakePartsCoordinates)
+                PrintSnakePart(i);
+            SnakeMove(direction);
+        }
+
+        private void SetInitialParts()
+        {
+            //setting the coordinates of the first 4 parts of the snake (the game starts with 4 snake parts)
+            snakePartsCoordinates.Add(new Point(100, 100));
+
+            for(int i = 0; i<4; i++)
+                snakePartsCoordinates.Add(new Point(snakePartsCoordinates[i].X + 10, snakePartsCoordinates[i].Y));
+        }
+
+        private void PrintSnakePart(Point coordinates)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Fill = Brushes.Black;
+            rect.Width = snakeSize;
+            rect.Height = snakeSize;
+
+            canvas.Children.Add(rect);
+
+            Canvas.SetLeft(rect, coordinates.X);
+            Canvas.SetTop(rect, coordinates.Y);
+
+        }
+
+        private void SnakeMove(string direction)
+        {
+            if(direction.Equals("right"))
+            {
+                Point lastPartCoordinates = snakePartsCoordinates[snakePartsCoordinates.Count - 1]; //saving the snake head's coordinates
+
+                snakePartsCoordinates.Add(new Point(lastPartCoordinates.X + snakeSize, lastPartCoordinates.Y)); //adding a new part next to the head (the last part in the list) of the snake
+                snakePartsCoordinates.Remove(snakePartsCoordinates[0]); //removing the last part of the snake (the tail)(the first in the list)
+                
+            }
         }
     }
 }
